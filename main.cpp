fbd213ddc3ce7b78eb44a3ebf11e75cf38a5ed95
Brett Hartman <brett.hartman@mac.com>
2019-03-23 08:01:52 -0700

    Implement functions to print intro and to get a guess

--- main.cpp
+++ main.cpp
@@ -1,3 +1,33 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
 int main() {
-  //
+  printIntro();
+
+  string guess = getGuess();
+
+  cout << "Your guess was: " << guess << endl;
+
+  return 0;
+}
+
+void printIntro() {
+  constexpr int WORD_LENGTH = 5;
+
+  cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+  cout << "Can you guess the " << WORD_LENGTH
+       << " letter isogram I'm thinking of?" << endl;
+  cout << endl;
+}
+
+string getGuess() {
+  cout << "Enter your guess: ";
+
+  string guess;
+
+  getline(cin, guess);
+
+  return guess;
 }
---
+++ main.h
@@ -0,0 +1,11 @@
+//
+// Created by Brett Hartman on 2019-03-23.
+//
+
+#ifndef BULLCOWGAME_MAIN_H
+#define BULLCOWGAME_MAIN_H
+
+void printIntro();
+std::string getGuess();
+
+#endif // BULLCOWGAME_MAIN_H
